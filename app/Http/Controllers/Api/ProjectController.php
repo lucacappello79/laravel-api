34aed87e01c4a210c085dacd409dd31f3480234c
lucacappello79 <postt79@gmail.com>
2023-05-29 23:27:16 +0200

    feat: updated api/projcontr/index with with type

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -11,7 +11,8 @@ class ProjectController extends Controller
     public function index()
     {
 
-        $projects = Project::all();
+        // $projects = Project::all();
+        $projects = Project::with('type')->get();
 
         return response()->json([
 
